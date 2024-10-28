5cbf535645901d0c8cb9188cc6bb09eed3c6bc79
thisistisha <tishachauhan8603@gmail.com>
2024-10-28 18:45:38 -0400

    COMP3095 - Week_6 Day-1 docker-compose.yml

---
+++ microservice-head/init/mongo/docker-entrypoint-initdb.d/mongo-init.js/mongo-init.js
@@ -0,0 +1,22 @@
+/** to create actual database and to assign privilege to the database **/
+
+print('START');
+
+
+db = db.getSiblingDB('product-service');
+
+db.createUser(
+    {
+        user: 'admin',
+        pwd: 'password',
+        roles: [ {role: 'readWrite', db: 'product-service'}]
+    }
+
+)
+
+db.createCollection('user');
+
+
+
+
+print('END');
